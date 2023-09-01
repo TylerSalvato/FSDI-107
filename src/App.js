6edcb70fe52c6d9a240a9294bcf5b261f46e2618
TylerSalvato <133399190+TylerSalvato@users.noreply.github.com>
2023-09-01 01:10:43 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './components/footer';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello from React </h1>
+      <Navbar/>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
+
+//create the footer componenet and import the footer component, please
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+function Navbar()
+{
+    return (
+        <div className="navbar">
+            menu goes here
+        </div>
+    )
+
+}
+
+export default Navbar;
+
